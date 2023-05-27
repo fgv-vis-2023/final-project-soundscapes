4dbbe3145198c822b7f64a5df0ff14304ffdf3af
cristiano.leal_ntconsult <junior.larrea.ids@gmail.com>
2023-05-27 16:13:04 -0300

    fix in html, downloading db

--- scripts/page2.js
+++ scripts/page2.js
@@ -1,2 +1,5 @@
-// https://raw.githubusercontent.com/fgv-vis-2023/final-project-soundscapes/main/data/songs.csv
-// Cristiano visualization
+d3.csv("https://raw.githubusercontent.com/fgv-vis-2023/final-project-soundscapes/main/data/songs.csv", function(data) {
+    for (var i = 0; i < 5; i++) {
+        console.log(data[i]);
+    }
+});
