46f473f889a04665fbb02be5a4e65020a8217813
Paloma <paloma.borges@fgv.edu.br>
2023-06-11 00:24:38 -0300

    add scatterplot by genre

--- scripts/page2.js
+++ scripts/page2.js
@@ -4,6 +4,21 @@ const MARGIN_LEFT = 100;
 const MARGIN_RIGHT = 30;
 const MARGIN_TOP = 10;
 const MARGIN_DOWN = 40;
+let data;
+
+var svg = d3.select('#genresGraph')
+    .append("svg")
+    .attr('width', width)
+    .attr('height', height);
+
+// Define scales
+var xScale = d3.scaleLinear()
+    .range([50, width - 200])
+    .nice();
+
+var yScale = d3.scaleBand()
+    .range([420, 20])
+    .padding(0.2);
 
 // This function splits the records by genre in the "genre" column
 function cleanData(data) {
@@ -23,28 +38,16 @@ function cleanData(data) {
     return myData;
 }
 
-d3.csv("https://raw.githubusercontent.com/fgv-vis-2023/final-project-soundscapes/main/data/songs.csv").then(data => {
+d3.csv("https://raw.githubusercontent.com/fgv-vis-2023/final-project-soundscapes/main/data/songs.csv").then(csvData => {
     // Prepare data by splitting the string in the "genre" column
-    data = cleanData(data).filter(d => d.genre !== "set()");
+    data = cleanData(csvData).filter(d => d.genre !== "set()");
 
     var genreCount = d3.rollup(data, v => v.length, d => d.genre);
     var sortedData = Array.from(genreCount.entries()).sort((a, b) => d3.descending(b[1], a[1]));
 
-    // Define scales
-    var xScale = d3.scaleLinear()
-        .domain([0, d3.max(genreCount.values())])
-        .range([50, width - 200])
-        .nice();
-
-    var yScale = d3.scaleBand()
-        .domain(sortedData.map(d => d[0]))
-        .range([420, 20])
-        .padding(0.2);
-
-    var svg = d3.select('#section4')
-        .append("svg")
-        .attr('width', width)
-        .attr('height', height);
+    // Update scales' domains
+    xScale.domain([0, d3.max(genreCount.values())]);
+    yScale.domain(sortedData.map(d => d[0]));
 
     svg.append('g')
         .attr('transform', `translate(50, ${height - MARGIN_DOWN-50})`)
@@ -69,11 +72,17 @@ d3.csv("https://raw.githubusercontent.com/fgv-vis-2023/final-project-soundscapes
         .attr("stroke-width", 0)
         .on("mouseover", function (d) {
             d3.select(this)
-                .attr("stroke-width", 2);
+                .attr("stroke-width", 2)
+                .attr("fill", "darkgreen")
+                .attr("cursor", "pointer");
         })
         .on("mouseout", function (d) {
             d3.select(this)
-                .attr("stroke-width", 0);
+                .attr("stroke-width", 0)
+                .attr("fill", "#1DB954");
+        })
+        .on("click", function (event, d) {
+            handleClick(d);
         });
 
     svg.append("text")
@@ -98,3 +107,82 @@ d3.csv("https://raw.githubusercontent.com/fgv-vis-2023/final-project-soundscapes
         .append('title')
         .text(d => `Average Popularity: ${genreCount.get(d[0])}`);
 });
+
+// This function updates the scatter plot with the selected genre
+function handleClick(d) {
+    var selectedGenre = d[0];
+    console.log(selectedGenre);
+    var selectedData = data.filter(item => item.genre === selectedGenre);
+    console.log(selectedData);
+    // Call a function to update the chart with the selected data
+    updateScatterPlot(selectedData);
+}
+
+// Function to update the scatter plot
+function updateScatterPlot(selectedData) {
+    d3.select('#genresGraph svg').selectAll("*").remove(); // Remove the previous svg
+    svg.selectAll("circle").remove(); // Remove previous circles
+
+    // Update scales' domains
+    yScale = d3.scaleLinear()
+        .range([height - MARGIN_DOWN - 50, MARGIN_TOP])
+        .nice();
+    xScale.domain([0, 100]);
+    yScale.domain([0, 1]);
+
+    svg.append('g')
+        .attr('transform', `translate(50, ${height - MARGIN_DOWN-50})`)
+        .call(d3.axisBottom(xScale));
+
+    svg.append('g')
+        .attr('transform', `translate(${MARGIN_LEFT}, 0)`)
+        .call(d3.axisLeft(yScale));
+
+    var circles = svg.selectAll("circle")
+        .data(selectedData)
+        .join("circle")
+        .attr("cx", d => xScale(d.popularity)+50)
+        .attr("cy", d => yScale(d.energy))
+        .attr("r", 5)
+        .attr("fill", "#1DB954")
+        .attr("stroke", "white")
+        .attr("stroke-width", 1)
+        .on("mouseover", function (d) {
+            d3.select(this)
+                .attr("stroke-width", 2)
+                .attr("fill", "darkgreen")
+                .attr("r", 7)
+                // bring to front
+                .raise();
+        }
+        )
+        .on("mouseout", function (d) {
+            d3.select(this)
+                .attr("stroke-width", 1)
+                .attr("fill", "#1DB954")
+                .attr("r", 5);
+        }
+        );
+
+    svg.append("text")
+        .attr("x", MARGIN_LEFT)
+        .attr("y", MARGIN_TOP)
+        .attr("text-anchor", "left")
+        .attr("fill", "white")
+        .attr("font-weight", "bold")
+        .attr("font-size", "14px")
+        .text("Energy");
+
+    svg.append("text")
+        .attr("x", 1100)
+        .attr("y", height - MARGIN_DOWN - 50)
+        .attr("text-anchor", "middle")
+        .attr("fill", "white")
+        .attr("font-weight", "bold")
+        .attr("font-size", "14px")
+        .text("Popularity");
+
+    circles
+        .append('title')
+        .text(d => `${d.song}\nby ${d.artist} \nPopularity: ${d.popularity} \nEnergy: ${d.energy}`);
+}
